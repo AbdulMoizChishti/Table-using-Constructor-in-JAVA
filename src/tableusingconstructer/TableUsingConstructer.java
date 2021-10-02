8692c9dd2bb6bc52e53e85ecc292c2d00e294c5e
AbdulMoizChishti <moaiz599@gmail.com>
2021-10-03 01:14:34 +0500

    Multiplicative Table

---
+++ src/tableusingconstructer/TableUsingConstructer.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tableusingconstructer;
+
+/**
+ *
+ * @author Abdul Moiz Chishti
+ */
+public class TableUsingConstructer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    Table obj=new Table();
+        
+    obj.table(7);
+    
+    
+    
+    
+    }
+    
+}
